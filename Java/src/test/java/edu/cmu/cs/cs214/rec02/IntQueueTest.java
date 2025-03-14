b6b3b5fcd78679e469d66ada545c423e2cf06c76
Nonsense727 <amarmend.11b@gmail.com>
2025-03-14 17:36:35 +0800

    zassan1

--- Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
+++ Java/src/test/java/edu/cmu/cs/cs214/rec02/IntQueueTest.java
@@ -42,7 +42,7 @@ public class IntQueueTest {
   @Test
     public void testNotEmpty() {
     mQueue.enqueue(1);
-    assertTrue(mQueue.isEmpty());
+    assertFalse(mQueue.isEmpty());
   }
 
   @Test
